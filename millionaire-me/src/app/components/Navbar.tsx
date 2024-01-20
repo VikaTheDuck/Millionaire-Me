c37691093cb57d08da6e92be5bd4ee194d259c15
Victoria Solodovnikova <victoria.solodovnikova@cae.com>
2024-01-20 12:49:44 -0500

    Project creation

---
+++ millionaire-me/src/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+// components/Navbar.tsx
+const Navbar = () => {
+    return (
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
