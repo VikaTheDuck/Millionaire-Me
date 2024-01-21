f07b8846b9327a85bb2b0bdc99b4f04402c82475
DuaAditya <adityaduacan@gmail.com>
2024-01-21 02:17:20 -0500

    Edits for the previous iteration of the request.

--- backend/calculateSavings.js
+++ backend/calculateSavings.js
@@ -3,11 +3,11 @@ function calculateSavings(disposableIncome, monthlyExpense) {
     let months = 0;
     const targetMultiplier = 6;
     const maxAge = 70*12;
-    // Loop to check if the person has enough savings
+    // Loop to check if the person has enough savings.
     while (savings < (targetMultiplier * monthlyExpense)) {
         savings += disposableIncome;
         months += 1;
-        // 
+        // If the number of months is larger than the max age then it will throw an error.
         if (months > maxAge) {
             throw new error(
                 "The average life expectency is 80. If you are at 70 and don't have a" +
@@ -17,8 +17,10 @@ function calculateSavings(disposableIncome, monthlyExpense) {
     }
 
     if (months > 6) {
-        // Convert months to years as an integer
+        // Convert months to years as an integer.
         const years = Math.round(months / 12);
         return years;
     }
 }
+
+export default calculateSavings;
