5813110665fb27f4a9dfbd17895b6651d70e42fa
Gabriel Martinica <gabmartinica@gmail.com>
2024-01-21 03:29:35 -0500

    Add data to get endpoint

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 let storedData = {};
 
 app.get("/info", (req, res) => {
-    res.json(storedData);
+    res.json({ data: storedData });
 });
 
 app.post("/calculate", (req, res) => {
