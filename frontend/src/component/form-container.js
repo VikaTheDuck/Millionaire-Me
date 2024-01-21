7a2bada15016b9e7be1eee9e471da817b99f4fc3
Victoria Solodovnikova <victoria.solodovnikova@cae.com>
2024-01-21 04:02:06 -0500

    correct placeholders

--- frontend/src/component/form-container.js
+++ frontend/src/component/form-container.js
@@ -47,58 +47,65 @@ const FormContainer = () => {
   };
 
   return (
-    <div className="w-1/3 mx-auto justify-center ">
+    <div className="w-full sm:w-2/3 md:w-2/3 lg:w-1/3 xl:w-1/3 mx-auto my-8">
       <div className="bg-gray-200 p-4 rounded-md shadow-md">
         <div className="justify-center">
-        <h2 className="pt-5 pb-3 text-4xl mb-4 text-center font-sans">Your Info</h2>
+        <h2 className="pt-4 pb-3 text-l mb-4 text-center font-medium">Your Info</h2>
         </div>
         <form onSubmit={handleSubmit} className="justify-center">
           <FormRow
             label="Age"
             type="number"
+            placeholder="0"
             value={formData.age}
             onChange={(e) => handleInputChange('age', e.target.value)}
           />
           <FormRow
             label="Savings"
             type="number"
+            placeholder="$"
             value={formData.savings}
             onChange={(e) => handleInputChange('savings', e.target.value)}
           />
           <FormRow
             label="Debt"
             type="number"
+            placeholder="$"
             value={formData.debt}
             onChange={(e) => handleInputChange('debt', e.target.value)}
           />
           <FormRow
             label="Debt Interest"
             type="number"
+            placeholder="%"
             value={formData.debtInterest}
             onChange={(e) => handleInputChange('debtInterest', e.target.value)}
           />
           <FormRow
             label="Debt Time to pay off"
             type="number"
+            placeholder="0"
             value={formData.debtTime}
             onChange={(e) => handleInputChange('debtTime', e.target.value)}
           />
           <FormRow
             label="Income"
             type="number"
+            placeholder="$"
             value={formData.income}
             onChange={(e) => handleInputChange('income', e.target.value)}
           />
           <FormRow
             label="Monthly Spending"
             type="number"
+            placeholder="$"
             value={formData.monthlySpend}
             onChange={(e) => handleInputChange('monthlySpend', e.target.value)}
           />
           <div class="flex justify-center pl-3 p-4 pt-6">
           <button
             type="submit"
-            class="px-4 py-1.5 flex-grow rounded-md shadow-lg bg-gradient-to-r from-green-400 to-lime-400 font-medium text-gray-100 block transition duration-300"
+            class="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 px-4 py-2 rounded-md shadow-lg bg-gradient-to-r from-green-400 to-lime-400 font-medium text-gray-100 transition duration-300"
           >
             Submit
           </button>
--- frontend/src/component/form-row.js
+++ frontend/src/component/form-row.js
@@ -1,24 +1,28 @@
 // components/FormRow.js
 import React from 'react';
 
-const FormRow = ({ label, type, value, onChange, infoText, onInfoClick }) => {
+const FormRow = ({ label, type, value, placeholder, onChange, infoText, onInfoClick }) => {
   return (
-    <div class="flex">
-    <div class="m-auto">
-      <label htmlFor={`exampleInput-${label}`} className="block pt-2 text-sm font-medium leading-6 text-gray-900">
+    <div class="grid grid-cols-1 pb-2 gap-4 sm:grid-cols-2">
+    {/* <div class="m-auto"> */}
+      <div>
+      <label htmlFor={`exampleInput-${label}`} className="block pl-10 text-sm font-medium leading-6 text-gray-900">
         {label}
       </label>
+      </div>
+      <div>
       <div className="flex items-center">
          <input
           type={type}
           id={`exampleInput-${label}`}
           className="border p-1 w-full text-sm w-40 rounded-lg justify-center"
-          placeholder={"0.00$"}
+          placeholder={placeholder}
           value={value}
           onChange={onChange}
         />
       </div>
-    </div>
+      </div>
+    {/* </div> */}
     </div>
   );
 };
