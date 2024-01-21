cfa0c320ba18b060021053214972d8bd63cf1f10
DuaAditya <adityaduacan@gmail.com>
2024-01-21 02:11:35 -0500

    Edits for the last commit for calculateInvesting and calculateSavings

--- backend/calculateInvesting.js
+++ backend/calculateInvesting.js
@@ -1,16 +1,20 @@
-function calculateInvesting(monthlyInvestment, annualInterestRate, inflationRate) {
+function calculateInvesting(monthlyInvestment, annualInterestRate, inflationRate, targetAmount) {
     let investments = 0;
     let months = 0;
 
-    while (investments < 1000000) {
+    // Loop to check the number of months it will take to reach the target amount.
+    while (investments < targetAmount) {
         investments += monthlyInvestment;
         investments *= (1 + annualInterestRate / 12);
         investments /= (1 + inflationRate / 12);
         months += 1;
     }
 
-    const years = months / 12;
-    return years;
+
+    if (months > 6) {
+        // Convert months to years as an integer
+        const years = Math.round(months / 12);
+        return years;
 }
 
 export default calculateInvesting;
--- backend/calculateSavings.js
+++ backend/calculateSavings.js
@@ -1,17 +1,24 @@
-function calculateSavings(monthlyInvestment, monthlyExpense) {
+function calculateSavings(disposableIncome, monthlyExpense) {
     let savings = 0;
     let months = 0;
-    while (savings < (6 * monthlyExpense)) {
-        savings += monthlyInvestments;
+    const targetMultiplier = 6;
+    const maxAge = 70*12;
+    // Loop to check if the person has enough savings
+    while (savings < (targetMultiplier * monthlyExpense)) {
+        savings += disposableIncome;
         months += 1;
+        // 
+        if (months > maxAge) {
+            throw new error(
+                "The average life expectency is 80. If you are at 70 and don't have a" +
+                " budget then please be okay with not being a millionaire."
+            )
+        }
     }
+
     if (months > 6) {
         // Convert months to years as an integer
-        const years = Math.floor(months / 12);
+        const years = Math.round(months / 12);
         return years;
-    } else {
-        return 0;
     }
 }
-
-export default calculateSavings;
